4825ed0f57511f8bca374aaeb36f29384bc169f4
khalil-elqaddabi <kahlilelqaddabi9@gmail.com>
2026-02-03 20:33:41 +0100

    feat:create_model_of_products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+           $products = Product::all();
+        return view("products.index", compact("products"));
     }
 
     /**
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
-    use HasFactory;
+    
+      use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'supplier_id',
+        'name',
+        'description',
+        'stock',
+        'price',
+        'reference',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
 }
