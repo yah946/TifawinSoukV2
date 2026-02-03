8b240b967aaf60617e8b7b65ce40afd988187c80
khalil-elqaddabi <kahlilelqaddabi9@gmail.com>
2026-02-03 20:33:41 +0100

    feat:create_model_of_products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
@@ -37,5 +38,27 @@ class Product extends Model
     public function orderItems(): HasMany
     {
         return $this->hasMany(OrderItem::class);
+    }
+    
+      use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'supplier_id',
+        'name',
+        'description',
+        'stock',
+        'price',
+        'reference',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
     }
 }
