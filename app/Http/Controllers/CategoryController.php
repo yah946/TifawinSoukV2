96e1ae29598693f28b1ea518b40f318c4e20dc46
mohamed <mojahdontheway@gmail.com>
2026-02-04 08:24:43 +0100

    add Controller edit for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -59,7 +59,7 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+         return view('categories.edit', compact('category'));
     }
 
     /**
