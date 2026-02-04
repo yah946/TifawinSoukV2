4903f1e0f7b3f567f86df3f520dfade8db8b7032
mohamedsehrane314@gmail.com <ton.email@example.com>
2026-02-04 05:45:21 +0100

    feat:add fillable variable and relation with product method

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Image extends Model
 {
     /** @use HasFactory<\Database\Factories\ImageFactory> */
     use HasFactory;
+
+    protected $fillable = ['product_id','path','cover'];
+    public function product(){
+        return $this->belongsTo(Product::class)
+    }
 }
