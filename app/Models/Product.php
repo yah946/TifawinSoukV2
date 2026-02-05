584723f9f896abd1d4ef1e8a6bc1130331d02ff1
khalil-elqaddabi <kahlilelqaddabi9@gmail.com>
2026-02-05 11:33:50 +0100

    solve conflict

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,33 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     
       use HasFactory, SoftDeletes;
-
-    protected $fillable = [
-        'category_id',
-        'supplier_id',
-        'name',
-        'description',
-        'stock',
-        'price',
-        'reference',
-    ];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function supplier()
-    {
-        return $this->belongsTo(Supplier::class);
-    use HasFactory;
 
 
     public function orderItems(): HasMany
