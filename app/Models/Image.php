580d7d5c9ee53444f3e52e6c56bc333cba58530f
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2026-02-05 19:02:58 +0100

    feat(admin): ajouter la gestion complète des fournisseurs dans l'interface d'administration
    - Création des routes pour les opérations CRUD des fournisseurs dans le module admin
    - Mise en place des vues Blade pour lister, créer, modifier et afficher les détails des fournisseurs
    - Intégration de la gestion de la corbeille avec restauration et suppression définitive
    - Ajout des relations entre modèles avec typage explicite des relations Eloquent
    - Mise à jour des dépendances de développement avec Laravel IDE Helper et Debugbar
    - Création de la structure de pagination et de filtrage pour la liste des fournisseurs
    - Implémentation des validations et gestion des erreurs dans les formulaires
    - Configuration de Debugbar pour faciliter le débogage en environnement de développement

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,6 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $product_id
+ * @property string $path
+ * @property int $cover
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Product $product
+ * @method static \Database\Factories\ImageFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Image newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Image newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Image query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Image whereCover($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Image whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Image whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Image wherePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Image whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Image whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Image extends Model
 {
     /** @use HasFactory<\Database\Factories\ImageFactory> */
