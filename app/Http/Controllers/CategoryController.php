1dcb135a3f646430834de6b1acc82f38685342e8
mohamed <mojahdontheway@gmail.com>
2026-02-05 12:27:21 +0100

    fix(controller):generate slug from name and random integer

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -30,20 +30,20 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'emoji' => 'nullable|string|max:10',
+        $validated = $request->validate([
+            'name' => 'required|string|max:70',
+            'emoji' => 'nullable|string|max:255',
             'description' => 'nullable|string',
         ]);
 
         Category::create([
             'name' => $request->name,
             'emoji' => $request->emoji,
-            'slug' => Str::slug($request->name),
+            'slug' => str_replace(' ','_',str_replace(' & ',' ',$validated['name'])).random_int(1,1000),
             'description' => $request->description,
         ]);
 
-        return redirect()->route('categories.index');
+        // return redirect(route('categories.index'));
     }
 
     /**
