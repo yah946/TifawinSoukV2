f72d313eccfdfde54fe89bc6cb051a97ad5c66fe
mohamedsehrane314@gmail.com <ton.email@example.com>
2026-02-06 17:10:43 +0100

    feat(views):the main page  display all products

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,8 +11,8 @@
 
 </head>
 
-<body>
-    <nav class="h-20 flex items-center justify-around">
+<body class="space-y-6 bg-gray-50">
+    <nav class="h-20 bg-white flex items-center justify-around">
         <h1 class="text-2xl font-thin">Tifawin Souk</h1>
         <div>
             <input
@@ -27,14 +27,48 @@
         </div>
     </nav>
     <main>
-        <section>
-            <div>
-                {{-- @foreach ($categories as $category)
-                    <a href="#">$category</a>
-                @endforeach --}}
+        <section class="flex gap-4 max-w-6xl mx-auto rounded py-2 px-4">
+            <div class="flex flex-col bg-white max-w-fit mx-auto rounded border border-gray-200 shadow-sm py-2 px-4">
+                @foreach ($categories as $category)
+                <div class="pb-2">
+                    <a class="hover:text-orange-500 text-sm" href="#">{{$category->name}}</a>
+                </div>
+                @endforeach
             </div>
-            <div></div>
-            <div></div>
+            <div style="background-image: url('/images/SX.jpg');" class="flex-1 rounded bg-cover bg-center" >
+            </div>
+            <div class="flex flex-col max-w-fit gap-2 ">
+                <div class="flex-1 bg-white rounded border border-gray-200 shadow-sm py-2 px-4">
+                    <div class="pb-2">
+                        <p class="text-sm">Centre d'assistance</p>
+                        <p class="text-gray-400 text-xs">Guide du service client</p>
+                    </div>
+                    <div class="pb-2">
+                        <p class="text-sm">WhatsApp</p>
+                        <p class="text-gray-400 text-xs">Discuter pour commander</p>
+                    </div>
+                    <div class="pb-2">
+                        <p class="text-sm">Vendez sur TifawinSouk</p>
+                        <p class="text-gray-400 text-xs">Ouvrez votre shop ici</p>
+                    </div>
+                </div>
+                <div class="flex-1 w-56 h-56">
+                    <img src="/images/TF.png" alt="">
+                </div>
+            </div>
+        </section>
+        <section class="max-w-6xl mx-auto rounded py-2 px-4">
+            <div class="bg-[#7b1fa2] w-full text-white text-2xl py-2 px-4 shadow-sm">Profitez des meilleurs deals</div>
+            <div class="flex flex-wrap gap-4 bg-white w-full text-2xl py-2 px-4 shadow-sm">
+                <article class="w-64 bg-white rounded-lg border border-gray-200 p-6 hover:shadow-md transition-shadow ">
+                    <a href="#">
+                        <div class="border border-black"><img src="/images/12.jpg" alt="cover"></div>
+                        <div class="text-sm text-gray-500 p-2 pb-1">Kraft line Machine</div>
+                        <div class="text-xs text-black p-2 pb-1">99.00 Dhs</div>
+                    </a>
+                </article>
+            </div>
+            
         </section>
     </main>
     <footer class="text-[#f2f2f2] bg-[#535357] max-w-full space-y-6 mx-auto p-4">
