c100132f36396cca5f1863268fab2c502f3d1a19
mohamedsehrane314@gmail.com <ton.email@example.com>
2026-02-06 11:22:40 +0100

    feat(index):nav bar and fotter as a layout to reuse

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('show');
+    return view('index');
 });
 
 Route::resource('products', ProductController::class);
