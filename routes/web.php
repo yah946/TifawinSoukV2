ccb4e961cb2c0455503da40929f46b2f0f14280f
khalil-elqaddabi <kahlilelqaddabi9@gmail.com>
2026-02-06 22:13:23 +0100

    fix:add_show_producy_path

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ProductController::class, 'index']);
 // Route::post('/', [ProductController::class, 'store'])->name('products.store');
 
-// Route::resource('products', ProductController::class);
+Route::get('/admin/products/{product}', [ProductController::class, 'show'])
+    ->name('products.show');
 
-// require_once __DIR__ ."/admin.php";
+Route::get('/admin/products/{product}/edit', [ProductController::class, 'edit'])
+    ->name('products.edit');
+
+Route::put('/admin/products/{product}', [ProductController::class, 'update'])
+    ->name('products.update');
+
+
+Route::delete('/admin/products/{product}', [ProductController::class, 'destroy'])
+    ->name('products.destroy');
+
+require_once __DIR__ ."/admin.php";
 
