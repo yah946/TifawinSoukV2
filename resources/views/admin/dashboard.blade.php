3fde81d5760562be0788716e941d3fde0d499868
khalil-elqaddabi <kahlilelqaddabi9@gmail.com>
2026-02-09 14:31:08 +0100

    feat:add_ui_products of view | create | insert

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,14 +15,14 @@
     </div>
 
     {{-- Categories --}}
-    <div class="bg-gray-100 hover:bg-green-200 transition rounded-2xl shadow-md p-6 flex flex-col items-center justify-center">
+    <div class="bg-gray-100 hover:bg-blue-200 transition rounded-2xl shadow-md p-6 flex flex-col items-center justify-center">
         <i class="fa-solid fa-list text-3xl mb-3 text-green-700"></i>
         <p class="text-gray-600">Total Categories</p>
         <p class="text-2xl font-bold">{{ $categoryCount }}</p>
     </div>
 
     {{-- Suppliers --}}
-    <div class="bg-gray-100 hover:bg-green-200 transition rounded-2xl shadow-md p-6 flex flex-col items-center justify-center">
+    <div class="bg-gray-100 hover:bg-orange-200 transition rounded-2xl shadow-md p-6 flex flex-col items-center justify-center">
         <i class="fa-solid fa-user-tie text-3xl mb-3 text-green-700"></i>
         <p class="text-gray-600">Total Suppliers</p>
         <p class="text-2xl font-bold">{{ $supplierCount }}</p>
