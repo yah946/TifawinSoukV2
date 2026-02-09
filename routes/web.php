d49d16c7d19e959c52a4a804d8b070e7e72c1638
mohamed <mojahdontheway@gmail.com>
2026-02-09 15:59:51 +0100

    fix:router issue

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CategoryController::class,'index']);
 
-Route::resource('products', ProductController::class);
 
 require_once __DIR__ . '/admin.php';
