6afbfe96ca0d291cd62b9b22c2c54853e54c82b8
mohamedsehrane314@gmail.com <ton.email@example.com>
2026-02-10 10:30:31 +0100

    feat:add search and filter by category and price

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
@@ -12,6 +13,26 @@ class ProductController extends Controller
     {
         return view('index',[
             'products' => Product::all(),
+            'categories' => Category::all()
+        ]);
+    }
+    public function filter(Request $request)
+    {
+        $products = Product::query()
+            ->when($request->search,function($q,$search){
+                $q->where('name','like','%{$search}%');
+            })
+            ->when($request->c,function($q,$category_id){
+                $q->where('category_id',$category_id);
+            })
+            ->when($request->min_price,function($q,$min){
+                $q->where('price','>=',$min);
+            })
+            ->when($request->max_price,function($q,$max){
+                $q->where('price','<=',$max);
+            })->get();
+        return view('index',[
+            'products' => $products,
             'categories' => Category::all()
         ]);
     }
